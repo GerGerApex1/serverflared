ad40fcae3f8b38ecd28770cdba775375b0f1ef3d
GerGerApex1 <71051211+GerGerApex1@users.noreply.github.com>
2025-12-23 14:30:16 +0800

    Combine legacyforge with forge

--- build.forge.gradle.kts
+++ build.forge.gradle.kts
@@ -1,9 +1,12 @@
 import org.gradle.kotlin.dsl.register
 
+ext["loom.platform"] = "forge"
 plugins {
 	id("mod-platform")
-	id("net.neoforged.moddev.legacyforge")
+	id("gg.essential.loom")
 }
+
+val minorVersion = "${prop("deps.minecraft")}".toString().split(".")[1].toInt()
 
 platform {
 	loader = "forge"
@@ -16,7 +19,28 @@ platform {
 		}
 	}
 }
-
+dependencies {
+	minecraft("com.mojang:minecraft:${prop("deps.minecraft")}")
+	forge("net.minecraftforge:forge:${property("deps.minecraft")}-${property("deps.forge")}")
+	// special handling for 16 i guess
+	println(minorVersion)
+	if (14 <= minorVersion) {
+		mappings(loom.officialMojangMappings())
+	} else {
+		mappings("de.oceanlabs.mcp:mcp_snapshot:${prop("deps.mcp")}@zip")
+	}
+	implementation(libs.jackson.dataformat.yaml)
+	implementation(libs.jackson.databind)
+}
+loom {
+	runConfigs {
+		remove(getByName("client"))
+	}
+	forge {
+		pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
+	}
+}
+/*
 legacyForge {
 	version = "${property("deps.minecraft")}-${property("deps.forge")}"
 
@@ -24,7 +48,7 @@ legacyForge {
 		register("server") {
 			server()
 			gameDirectory = file("run/")
-			ideName = "Forge Server (${stonecutter.active?.version})"
+			ideName = "Legacy Forge Server (${stonecutter.active?.version})"
 		}
 	}
 
@@ -35,18 +59,22 @@ legacyForge {
 		}
 	}
 }
+*/
 
-dependencies {
-	implementation(libs.jackson.dataformat.yaml)
-	implementation(libs.jackson.databind)
-
+repositories {
+	exclusiveContent {
+		forRepository {
+			maven {
+				url = uri("https://repo.spongepowered.org/maven/")
+			}
+		}
+		filter {
+			//includeGroup("de.oceanlabs.mcp") // only dependencies from this group
+		}
+	}
 }
-
 sourceSets {
 	main {
 	}
 }
 
-tasks.named("createMinecraftArtifacts") {
-	dependsOn(tasks.named("stonecutterGenerate"))
-}
