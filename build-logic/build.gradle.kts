4846641df647d01d48b5a460bdeeefac237d3d42
GerGerApex1 <71051211+GerGerApex1@users.noreply.github.com>
2026-02-15 05:57:46 +0800

    Remove fletching table & unused plugins

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -27,5 +27,5 @@ dependencies {
 	implementation(libs.kikugie.stonecutter)
 	implementation(libs.mod.publish.plugin)
 	implementation(libs.foojay.resolver)
-	implementation(libs.fletching.table)
+	//implementation(libs.fletching.table)
 }
