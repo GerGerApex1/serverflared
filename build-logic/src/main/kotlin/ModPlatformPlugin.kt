4a790367b653233c9e7632e56cf2be9d732671cb
GerGerApex1 <71051211+GerGerApex1@users.noreply.github.com>
2026-03-14 17:32:30 +0800

    chore (ci) (cd): add permissions for test build & use matrix strategy

--- build-logic/src/main/kotlin/ModPlatformPlugin.kt
+++ build-logic/src/main/kotlin/ModPlatformPlugin.kt
@@ -31,7 +31,7 @@ abstract class ModPlatformPlugin @Inject constructor() : Plugin<Project> {
 
 		val extension = extensions.create("platform", ModPlatformExtension::class.java).apply {
 			loader.convention(inferredLoader)
-			jarTask.convention("jar")
+			jarTask.convention("remapJar")
 			sourcesJarTask.convention("sourcesJar")
 		}
 
