2bf514f15426c9d2d9fd60c497b9bfacf1205c97
GerGerApex1 <71051211+GerGerApex1@users.noreply.github.com>
2026-03-14 21:17:05 +0800

    i give up

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -49,14 +49,14 @@ stonecutter parameters {
 	val isLegacyForge = minorVersion?.let { it <= 12 } ?: false
 	constants["legacy_forge"] = isLegacyForge
 	constants["release"] = property("mod.id") != "modtemplate"
-	replacements.string(current.parsed >= "1.18", "forge_imports_modern") {
+	replacements.string(current.parsed >= "1.17", "forge_imports_modern") {
 		replace("net.minecraftforge.fml.event.server", "net.minecraftforge.event.server")
 		replace("FMLServerStartedEvent", "ServerStartedEvent")
 		replace("FMLServerStartingEvent", "ServerStartingEvent")
 		replace("FMLServerStoppingEvent", "ServerStoppingEvent")
 	}
 	swaps["fml_serverlifecyclehooks_1_18"] = when {
-		eval(current.version, ">=1.18") -> "import net.minecraftforge.server.ServerLifecycleHooks;"
+		eval(current.version, ">=1.17") -> "import net.minecraftforge.server.ServerLifecycleHooks;"
 		else -> "import net.minecraftforge.fml.server.ServerLifecycleHooks;"
 	}
 }
